84317c89c9f22f542953c03ad11af723a3f24f37
Valentina Kirsanova <kirsanova.va@mobilcard.ru>
2020-03-02 18:45:39 +0300

    validation problem solved

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -10,6 +10,17 @@ function print(text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid(name) {
+  if (!name) {
+    return false;
+  }
+
+  const stringLength = name.length;
+
+  const lessThan4 = stringLength < 4;
+  const isEmpty = stringLength === 0;
+  const hasSpaces = name.includes(' ');
+
+  return !isEmpty && !lessThan4 && !hasSpaces;
 }
 
 function sayHello() {
