60b36f3bdf16aa4247ae451dc1c8177cfbb6a07b
Valentina Kirsanova <kirsanova.va@mobilcard.ru>
2020-03-02 19:20:36 +0300

    fixed spam checking; solved maxlength problem

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -5,5 +5,13 @@
  * @returns {string}
  */
 function truncate(str, maxlength) {
+  const strLength = str.length;
+  const longer = strLength > maxlength;
 
+  if (!longer) {
+    return str;
+  }
+
+  const copyStr = str.slice(0, maxlength - 1);
+  return `${copyStr}…`;
 }
