5576fd47fad847627d31dd515647e07f7bc570c1
Valentina Kirsanova <kirsanova.va@mobilcard.ru>
2020-03-03 11:49:50 +0300

    solved sumSalaries problem

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -4,5 +4,13 @@
  * @returns {Number}
  */
 function sumSalary(salaries) {
+  let sum = 0;
 
+  for (const key in salaries) {
+    if (typeof salaries[key] === 'number') {
+      sum += salaries[key];
+    }
+  }
+
+  return sum;
 }
