05a3339b81c4754b6b556c3e4823ba04b92878c3
ValentineKirsanova <valentine.kirsanova@gmail.com>
2020-03-21 15:21:09 +0300

    resolved min max string problem

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -4,4 +4,12 @@
  * @returns {{min:number, max:number}}  объект
  */
 function getMinMax(str) {
+  const numbers = str.split(', ')
+    .join(' ')
+    .split(' ')
+    .filter((item) => !isNaN(item));
+  return ({
+    min: Math.min(...numbers),
+    max: Math.max(...numbers)
+  });
 }
